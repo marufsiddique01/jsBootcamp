efa5b20ec752429e8696f047604a69bfb2b8d352
marufsiddique01 <marufsiddique71@yahoo.com>
2021-03-12 00:47:52 +0600

    welp :3 variable hierarchy

--- Scoping in practice/script.js
+++ Scoping in practice/script.js
@@ -1,1 +1,17 @@
 'use strict';
+
+//global function
+function calcAge(birthYear) {
+  const age = 2021 - birthYear;
+
+  function printAge() {
+    const output = ` You are ${age} years old, and you're born in ${birthYear}`;
+    console.log(output);
+  }
+  printAge();
+  return age;
+}
+
+//global varriable
+const firstName = 'Maruf';
+calcAge(1995);
