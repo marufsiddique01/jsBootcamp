6cd9654f001f13b21b93f6bd08f7b3f88661bbfe
marufsiddique01 <marufsiddique71@yahoo.com>
2021-04-14 02:02:56 +0600

    end of strings

--- data-structures/script.js
+++ data-structures/script.js
@@ -621,3 +621,27 @@ console.log(maskCreditCardNumber(4673198314));
 //repeat
 const message2 = 'Bad weather...All Departures Delayed... ';
 console.log(message2.repeat(5));
+
+// Challenge #4
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
+
+document.querySelector('button').addEventListener('click', function () {
+  const text = document.querySelector('textarea').value;
+  const rows = text.split('\n');
+  console.log(rows);
+
+  for (const [i, row] of rows.entries()) {
+    const [first, second] = row.toLowerCase().trim().split('_');
+    const output = `${first}${second.replace(
+      second[0],
+      second[0].toUpperCase()
+    )}`;
+    console.log(`${output.padEnd(20)} ${'✅'.repeat(i + 1)}`);
+  }
+});
+// underscore_case;
+// first_name;
+// Some_Variable;
+// calculate_AGE;
+// delayed_departure;
