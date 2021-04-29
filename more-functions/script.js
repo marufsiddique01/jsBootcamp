b4192ab941a8aa3e2ba81e08f5536925f03c3b69
marufsiddique01 <marufsiddique71@yahoo.com>
2021-04-30 01:10:50 +0600

    functions

--- more-functions/script.js
+++ more-functions/script.js
@@ -1,3 +1,26 @@
 'use strict';
 
+const bookings = [];
 //test
+const createBooking = function (
+  flightNum,
+  numPassengeers = 1,
+  price = 199 * numPassengeers
+) {
+  //ES5
+  //   numPassengeers = numPassengeers || 1;
+  //   price = price || 199;
+
+  const booking = {
+    flightNum,
+    numPassengeers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 800);
+createBooking('LH123', 5);
+createBooking('LH123');
